7f17fb1f998994127b95fe129a250d7997fee982
sahubhoomika7272-debug <sahubhoomika7272@gmail.com>
2026-01-09 14:52:38 +0530

    Initial commit: Sales & Revenue Analysis SQL Project

---
+++ Project 1: Sales & Revenue Analysis Using SQL/data.sql
@@ -0,0 +1,14 @@
+-- =========================================
+-- Sample Data for Sales Table
+-- =========================================
+
+INSERT INTO sales (sale_id, order_date, region, product, revenue, quantity) VALUES
+(1, '2024-01-10', 'North', 'Laptop', 75000.00, 1),
+(2, '2024-01-15', 'South', 'Mobile', 30000.00, 2),
+(3, '2024-02-05', 'East', 'Tablet', 20000.00, 1),
+(4, '2024-02-18', 'North', 'Mobile', 15000.00, 1),
+(5, '2024-03-01', 'West', 'Laptop', 80000.00, 1),
+(6, '2024-03-12', 'South', 'Tablet', 25000.00, 2),
+(7, '2024-03-20', 'East', 'Laptop', 72000.00, 1),
+(8, '2024-04-02', 'West', 'Mobile', 28000.00, 2);
+  
